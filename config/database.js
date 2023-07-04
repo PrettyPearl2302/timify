29b756a911415d89df072b61b57129c03792b8fb
Pearl Wogu <pearlw@meta.com>
2023-07-04 14:56:00 -0700

    Intial commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+    development: {
+      username: '<pearlwogu',
+      password: 'bernadette is my mom',
+      database: 'capstone',
+      host: 'localhost',
+      dialect: 'postgres',
+    },
+    production: {
+      // Configuration for production environment
+    },
+    // Add other environments as needed
+  };
